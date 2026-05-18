a19f1c793e0402a70dd855419a08ecb18e66ca14
guscsh <guschoish@gmail.com>
2026-05-18 14:34:18 +0800

    Step 6: listing template done

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
+from django.core.paginator import Paginator
 
 # Create your views here.
 def listings(request):
     listings = Listing.objects.all()
-    context = {'listings': listings}
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {'listings': paged_listings}
     return render(request,"listings/listings.html", context)
 
 def listing(request, listing_id):
-    return render(request,"listings/listing.html")
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {"listing": listing}
+    return render(request,"listings/listing.html", context)
