152061a44f13bc117142f05b960a16b92cd7fe08
guscsh <guschoish@gmail.com>
2026-05-22 16:24:34 +0800

    Step 11: website done

--- listings/models.py
+++ listings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from doctors.models import Doctor
 from .choices import district_choices, room_type_choices, rooms_choices
+from taggit.managers import TaggableManager
 # Create your models here.
 class Listing(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
@@ -9,7 +10,7 @@ class Listing(models.Model):
     district = models.CharField(max_length=50, choices=district_choices.items(), default="")
     choices = models.CharField(max_length=50)
     description = models.TextField(blank=True)
-    services = models.CharField(max_length=200)
+    services = TaggableManager(verbose_name="Services", blank=True)
     service = models.IntegerField()
     room_type = models.CharField(max_length=50, choices=room_type_choices.items(), default="")
     rooms = models.IntegerField(choices=rooms_choices, default=1)
